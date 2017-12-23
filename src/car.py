1002aa734a7bac09a54756edc5d5c7537a2c6779
Your Name <you@example.com>
2017-12-23 03:59:34 +0000

    add

--- src/car.py
+++ src/car.py
@@ -26,8 +26,9 @@ class Car:
 
         self.PWM_HZ = pwm_hz
         settings_f = os.path.join(os.path.abspath(__file__), 'settings.txt')
+	print("what is fucking settings:", settings_f)
         if os.path.exists(settings_f):
-            setting = np.loadtxt("./settings.txt")
+            setting = np.loadtxt(settings_f)
             self.halfL = setting[0][1] + setting[0][0] * 50
             self.fullL = setting[0][1] + setting[0][0] * 100
             self.halfR = setting[1][1] + setting[1][0] * 50
